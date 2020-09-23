1997817ba4df221d5bff74c1e64316445555cbbf
megachweng <megachweng@gmail.com>
2020-09-23 19:35:32 +0800

    test polygon osm and new style files

--- compile_map.sh
+++ compile_map.sh
@@ -1,5 +1,5 @@
 mkgmap="bin/mkgmap-r4581/mkgmap.jar"
-stylefile="styles/CF_Mapsource"
+stylefile="styles/Type_Indicator"
 optionsFile="mkgmap_options/mgc.args"
 typfile="typ/mgcmpb-draworder_modified.TYP"
 resources_dir="resources"
@@ -11,8 +11,8 @@ rm -rf $outdir/*
 
 java -DLog.config=logging.properties -Xmx4g -ea -jar $mkgmap \
 --style-file=$stylefile  \
---precomp-sea=$resources_dir/sea \
 --bounds=$resources_dir/bounds-latest \
 --output-dir=product \
--c $optionsFile \
-$typfile
+-c $optionsFile
+# $typfile
+# --precomp-sea=$resources_dir/sea
--- split_tile.sh
+++ split_tile.sh
@@ -1,6 +1,6 @@
 PATH_TO_SPLITTER="bin/splitter-r597/splitter.jar"
 PROJECT_DIR="$HOME/project/gmap_osm_mgc"
-TILE_FILE="$PROJECT_DIR/tiles/test_zhejiang.pbf"
+TILE_FILE="$PROJECT_DIR/tiles/test_nodes.osm"
 OUTDIR="$PROJECT_DIR/splitted"
 RESOURCES_DIR="$PROJECT_DIR/resources"
 PRECOMP_SEA_DIR="$RESOURCES_DIR/sea"
@@ -13,6 +13,7 @@ rm -rf $OUTDIR/*
 java -Xmx2000m -DLog.config=logging.properties \
 -ea -jar $PATH_TO_SPLITTER \
 --max-nodes=$MAX_NODES \
---precomp-sea=$PRECOMP_SEA_DIR \
---geonames-file=$GEO_NAME_FILE \
---output-dir=$OUTDIR $TILE_FILE
+--output-dir=$OUTDIR \
+$TILE_FILE
+
+ #--precomp-sea=$PRECOMP_SEA_DIR --geonames-file=$GEO_NAME_FILE \
