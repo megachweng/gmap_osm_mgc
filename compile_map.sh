ba41e82d94b3de07f0d8217952848af2ab95e896
megachweng <megachweng@gmail.com>
2020-09-27 18:00:24 +0800

    MGC_EDGE

--- compile_map.sh
+++ compile_map.sh
@@ -1,18 +1,18 @@
 mkgmap="bin/mkgmap-r4581/mkgmap.jar"
-stylefile="styles/Type_Indicator"
-optionsFile="mkgmap_options/mgc.args"
-typfile="typ/mgcmpb-draworder_modified.TYP"
+stylefile="styles/MGC_Edge"
+optionsFile="mgc.args"
+typfile="typ/MGC_Edge.TYP"
 resources_dir="resources"
 outdir="product"
 
-echo "$outdir"
-
 rm -rf $outdir/*
 
-java -DLog.config=logging.properties -Xmx4g -ea -jar $mkgmap \
---style-file=$stylefile  \
---bounds=$resources_dir/bounds-latest \
---output-dir=product \
--c $optionsFile
-# $typfile
-# --precomp-sea=$resources_dir/sea
+java -DLog.config=logging.properties -Xmx10g -ea -jar $mkgmap \
+  --style-file=$stylefile \
+  --max-jobs=12 \
+  --bounds=$resources_dir/bounds-latest \
+  --output-dir=product \
+  --bounds=$resources_dir/bounds-latest \
+  --precomp-sea=$resources_dir/sea \
+  -c $optionsFile \
+  $typfile
--- split_tile.sh
+++ split_tile.sh
@@ -1,6 +1,6 @@
 PATH_TO_SPLITTER="bin/splitter-r597/splitter.jar"
 PROJECT_DIR="$HOME/project/gmap_osm_mgc"
-TILE_FILE="$PROJECT_DIR/tiles/test_nodes.osm"
+TILE_FILE="$PROJECT_DIR/tiles/test_hangzhou.pbf"
 OUTDIR="$PROJECT_DIR/splitted"
 RESOURCES_DIR="$PROJECT_DIR/resources"
 PRECOMP_SEA_DIR="$RESOURCES_DIR/sea"
@@ -10,10 +10,11 @@ MAX_NODES=1600000
 mkdir -p $OUTDIR
 rm -rf $OUTDIR/*
 
-java -Xmx2000m -DLog.config=logging.properties \
--ea -jar $PATH_TO_SPLITTER \
---max-nodes=$MAX_NODES \
---output-dir=$OUTDIR \
-$TILE_FILE
-
- #--precomp-sea=$PRECOMP_SEA_DIR --geonames-file=$GEO_NAME_FILE \
+java -Xmx10g -DLog.config=logging.properties \
+  -ea -jar $PATH_TO_SPLITTER \
+  --max-nodes=$MAX_NODES \
+  --output-dir=$OUTDIR \
+  --precomp-sea=$PRECOMP_SEA_DIR \
+  --geonames-file=$GEO_NAME_FILE \
+  --polygon-file=china.poly \
+  $TILE_FILE
